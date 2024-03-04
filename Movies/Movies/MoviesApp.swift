4244ce337d12b697dbd4ca4ece8b0cbbd2bbad44
Jose96Luis <jose_luis1996@live.com.mx>
2024-03-04 14:39:49 -0600

    Primer commit

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by José Rodriguez Romero on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
